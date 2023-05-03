9c6e38d00ee588ac9bec828a0219f65aedc91821
avneet singh <96073704+avneet-singh843@users.noreply.github.com>
2023-05-03 13:43:54 +0530

    Update App.js
    corrected path

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import EventsPage from "./pages/EventsPage";
+import HomePage from "./Pages/HomePage";
+import EventsPage from "./Pages/EventsPage";
 import HeaderComponent from "./components/HeaderComponent";
 import FooterComponent from "./components/FooterComponent";
 function App() {
